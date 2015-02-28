4bd4a30d512a2cba6fbbf86120b3618b08e099a7
Dalibor Karlović <dado@burza.hr>
2015-02-28 12:01:07 +0100

    PoC

---
+++ complete.sh
@@ -0,0 +1,16 @@
+_foo() 
+{
+    local cur prev opts
+
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=`${COMP_WORDS[0]} --help`
+
+    # autocompleting options
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+}
+complete -F _foo foo
